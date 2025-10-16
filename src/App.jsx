ee9b6ef5c69e917d0f1f6ff2880d00856d806b3a
collinsbartholomew <collinsomega177@gmail.com>
2025-10-16 03:20:43 +0000

    implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import Hero from "./components/Hero.jsx";
 import NavBar from "./components/NavBar.jsx";
 
 function App() {
 	return (
 		<main>
-			{" "}
-			<NavBar></NavBar>
-			<h1 className={`text-3xl font-black tracking-[10px]`}> </h1>
+			<NavBar />
+			<Hero />
 		</main>
 	);
 }
